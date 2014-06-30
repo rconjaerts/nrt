37add2274cc0e92c612000f7835673c678351a36
Tom Jaspers <tomjaspers@gmail.com>
2014-06-30 19:59:50 +0200

    pizza

--- raspy/handle_sound.py
+++ raspy/handle_sound.py
@@ -15,7 +15,8 @@ value = 100 * (amplitude - threshold) / (1-threshold)
 url = 'http://192.168.137.1:8080/BigSister/webresources/entities.eventaudio'
 payload = {'timestamp': int(time.time()), 
           'value': value,
-          'accountId': 1}
+          'accountId': 1,
+          'type_id': 1,}
 headers = {'content-type': 'application/json'}
 
 r = requests.post(url, data=json.dumps(payload), headers=headers)
---
+++ raspy/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo /home/pi/raspy/noise_detection.rb -m 1 -v
+sudo motion -n /home/pi/raspy/motion.conf
