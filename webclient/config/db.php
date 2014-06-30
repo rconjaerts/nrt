b7c5b656c4e1ef80f4a94eb6f2adfb919990ad89
Tom Jaspers <tomjaspers@gmail.com>
2014-06-30 21:48:11 +0200

    initial webclient

---
+++ webclient/config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=134.184.49.7;port=33060;dbname=openis',
+    'username' => 'OpenIs',
+    'password' => 'OpenIs#FFF',
+    // 'dsn' => 'mysql:host=localhost;dbname=openis',
+  //   'username' => 'root',
+  //   'password' => 'root',
+    'charset' => 'utf8',
+];
---
+++ webclient/views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm; 
+/**
+ * @var yii\web\View $this
+ * @var $rows
+ */
+$this->title = 'Test';
+?>
+<div class="site-index">
+	
+</div>
+
+
