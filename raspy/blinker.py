0586715524ecaa46f5adf5254cc551ffc8cb3c92
Tom Jaspers <tomjaspers@gmail.com>
2014-07-01 02:20:58 +0200

    better blinker

--- raspy/blinker.py
+++ raspy/blinker.py
@@ -1,20 +1,24 @@
+#!/usr/bin/python
 import RPi.GPIO as GPIO  
 import time  
 
 # blinking function  
-def blink(pin):  
-        GPIO.output(pin,GPIO.HIGH)  
-        time.sleep(1)  
-        GPIO.output(pin,GPIO.LOW)  
-        time.sleep(1)  
+def blink(pin, sleeptime=0.5, blinks=1): 
+        for i in range(0,blinks):
+            GPIO.output(pin,GPIO.HIGH)  
+            time.sleep(sleeptime)  
+            GPIO.output(pin,GPIO.LOW)  
+            time.sleep(sleeptime)  
         return  
         
-# to use Raspberry Pi board pin numbers  
-GPIO.setmode(GPIO.BOARD)  
-# set up GPIO output channel  
-GPIO.setup(11, GPIO.OUT)  
+        
+def setup():
+    # to use Raspberry Pi board pin numbers  
+    GPIO.setmode(GPIO.BOARD)  
+    
+    # set up GPIO output channel  
+    GPIO.setup(11, GPIO.OUT)  
 
-# blink GPIO17 50 times  
-for i in range(0,50):  
-        blink(11)  
-GPIO.cleanup()   
+    # blink GPIO17 50 times  
+    blink(11, 0.5, 50)  
+    GPIO.cleanup()   
