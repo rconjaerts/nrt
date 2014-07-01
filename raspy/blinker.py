581babc155bf80070252519b916fea8e99e8b321
Tom Jaspers <tomjaspers@gmail.com>
2014-07-01 02:23:53 +0200

    blinekr

--- raspy/blinker.py
+++ raspy/blinker.py
@@ -4,21 +4,13 @@ import time
 
 # blinking function  
 def blink(pin, sleeptime=0.5, blinks=1): 
+        GPIO.setmode(GPIO.BOARD)  
         for i in range(0,blinks):
             GPIO.output(pin,GPIO.HIGH)  
             time.sleep(sleeptime)  
             GPIO.output(pin,GPIO.LOW)  
-            time.sleep(sleeptime)  
+            time.sleep(sleeptime)
+        GPIO.cleanup()   
         return  
         
         
-def setup():
-    # to use Raspberry Pi board pin numbers  
-    GPIO.setmode(GPIO.BOARD)  
-    
-    # set up GPIO output channel  
-    GPIO.setup(11, GPIO.OUT)  
-
-    # blink GPIO17 50 times  
-    blink(11, 0.5, 50)  
-    GPIO.cleanup()   
