83b38450542fd8190b00d901375d3f6c9a3b7056
Tom Jaspers <tomjaspers@gmail.com>
2014-07-01 02:24:54 +0200

    </blink>

--- raspy/blinker.py
+++ raspy/blinker.py
@@ -5,12 +5,11 @@ import time
 # blinking function  
 def blink(pin, sleeptime=0.5, blinks=1): 
         GPIO.setmode(GPIO.BOARD)  
+        GPIO.setup(pin, GPIO.OUT)  
         for i in range(0,blinks):
             GPIO.output(pin,GPIO.HIGH)  
             time.sleep(sleeptime)  
             GPIO.output(pin,GPIO.LOW)  
             time.sleep(sleeptime)
         GPIO.cleanup()   
-        return  
-        
-        
+        return
